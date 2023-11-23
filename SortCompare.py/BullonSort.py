d114905a44aa9a1dced974803df0bb1b7cc4a0fa
烁曈 <138376327+liushuotong@users.noreply.github.com>
2023-11-24 07:25:07 +0800

    Delete SortCompare.py directory

--- SortCompare.py/BullonSort.py
+++
@@ -1,6 +0,0 @@
-def BullonSort(list):
-    for i in range(0,len(list)):
-        for j in range(i-2,len(list)-1):
-            if list[j] >= list[j+1]:
-                list[j],list[j+1] = list[j+1],list[j]
-    return list
--- SortCompare.py/randomSet.py
+++
@@ -1,6 +0,0 @@
-import random
-def randomSet(n):
-    list = []
-    for i in range(n):
-        list.append(random.randint(0,100))
-    return list
